65f8667cbd7f3738a48b45789ffa0f90ac15a178
Md Salman Rahman <60688419+salmanrahmannishat@users.noreply.github.com>
2020-04-29 17:28:13 +0600

    Update det_max_min_numbers.cpp

--- det_max_min_numbers.cpp
+++ det_max_min_numbers.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"Name: Md. Zubaer Ahammed\nRoll: 1201062\nBatch: 12\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)\n"<<endl;
+    cout<<"Name: Md. Salman Rahman\nRoll: 1301099\nSection: B\nGroup: B1\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)"<<endl;
     int i=0,j=0, a, b,n, c, numbers[200];
     cout<<"Input numbers as many as you wish and this program will find maximum and minimum from your numbers."<<endl;
     cout<<"\nHow many number do you want to input= "; cin>>n;
