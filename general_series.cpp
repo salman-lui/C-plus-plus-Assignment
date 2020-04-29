249d69f9c20562afd55d3dfc5820689c1e0dfa8d
Md Salman Rahman <60688419+salmanrahmannishat@users.noreply.github.com>
2020-04-29 17:29:02 +0600

    Update general_series.cpp

--- general_series.cpp
+++ general_series.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"Name: Md. Zubaer Ahammed\nRoll: 1201062\nBatch: 12\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)"<<endl;
+    cout<<"Name: Md. Salman Rahman\nRoll: 1301099\nSection: B\nGroup: B1\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)"<<endl;
     int n, i, sum=0;
     cout<<"Enter the positive integer: ";
     cin>>n;
