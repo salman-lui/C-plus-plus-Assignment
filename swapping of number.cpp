05a9d5e2d172e4cc1b9067d715ffaf7f0be2ab3b
Md Salman Rahman <60688419+salmanrahmannishat@users.noreply.github.com>
2020-04-29 17:30:04 +0600

    Update swapping of number.cpp

--- swapping of number.cpp
+++ swapping of number.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"Name: Md. Zubaer Ahammed\nRoll: 1201062\nBatch: 12\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)\n"<<endl;
+    cout<<"Name: Md. Salman Rahman\nRoll: 1301099\nSection: B\nGroup: B1\nCourse No: CE-218\nCourse Title: Computer Programming (Sessional)"<<endl;
     int a, b;
     cout<<"Please enter the values of a and b: "<<endl;
     cout<<"a= "; cin>>a;
